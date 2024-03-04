551c91630b1106a06dc74268f39937e6ca70911a
djahasiel16 <djahasiel101600@gmail.com>
2024-03-04 16:57:34 +0800

    First Point
    this is the part for memorizing all the learnings
    from https://www.youtube.com/watch?v=SqcY0GlETPk&t=910s @58:18

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,27 @@
-import ListGroup from "./components/listGroup";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>;
+  let items = [
+    "Tubay",
+    "Cabadbaran City",
+    "Butuan City",
+    "Ampayon",
+    "RTR",
+    "Bayugan",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
